84112a9c1ab75e590517f77f3a76b2b587ab04bc
Brenda Milena Senilliani <bresen@alu.edu.gva.es>
2024-02-14 16:36:49 +0100

    Implemento componente para mostrar repositorios

--- src/components/GitHubRepo.vue
+++ src/components/GitHubRepo.vue
@@ -1,22 +1,20 @@
 <script setup>
 // Lo que acepta
 defineProps({
-   repo: Object
+  repo: Object
 })
 </script>
 
 <template>
-  <div class="list-group">
-    <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3 mt-4" aria-current="true">
-      <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
-      <div class="d-flex gap-2 w-100 justify-content-between">
-        <div>
-          <h6 class="mb-0">{{ repo }}</h6>
-          <p class="mb-0 opacity-75">Some placeholder content in a paragraph.</p>
-        </div>
-        <small class="opacity-50 text-nowrap">now</small>
+  <div class="list-group px-0 col-11 m-auto col-md-5 mt-0 mx-auto">
+    <div class="list-group-item list-group-item-action mt-4">
+      <div class="d-flex py-3 justify-content-between align-items-center">
+        <a :href="repo.html_url" target="_blank" class="fw-bold" :title="repo.description">
+          {{ repo.full_name }}
+        </a>
+        <small class="opacity-50 text-nowrap badge bg-secondary">{{ repo.forks_count }}</small>
       </div>
-    </a>
+    </div>
   </div>
 </template>
 
